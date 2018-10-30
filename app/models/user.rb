b91af88053d75e71c7dc1d63323f8b245ea4f630
stephan-dev <stephan-dev@outlook.fr>
2018-10-30 16:05:30 +0100

    ébauches

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+	def index
+		@user = User.all
+	end
+
+	def new
+		@user = User.new
+	end
+
+	def create
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+	get '/', to: 'Users#index'
+  get '/new', to:'Users#new'
+  post '/create', to: 'Users#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
